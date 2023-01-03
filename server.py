f41554b65ef083cc676a07e0d2cc3a4631d0b6db
meepowlz <meepowlz@gmail.com>
2023-01-02 23:14:44 -0500

    Add dashboard and login

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 # An instance of the Flask class is created
 app = Flask(__name__)
@@ -6,9 +6,17 @@ port = 8080
 
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/dashboard", methods=["GET", "POST"])
+def dashboard_page():
+    return render_template("base.html")
 
+
+@app.route("/login", methods=["GET", "POST"])
+def login_page():
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        return redirect(url_for("dashboard_page"))
 
 # Flask app is run, allowing access of the webpage
 app.run(host="localhost", port=port)
