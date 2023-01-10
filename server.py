80b82727425c523cea064009c7bd486f9ed938ac
meepowlz <meepowlz@gmail.com>
2023-01-09 19:46:43 -0500

    Change keys

--- server.py
+++ server.py
@@ -3,7 +3,8 @@ from dotenv import load_dotenv
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_session import Session
 from functools import wraps
-import users, events
+import users
+import events
 
 
 # Allows environment variables to be accessed
@@ -31,6 +32,7 @@ def check_session():
     return decorator
 
 
+# (Temp) resets login info for testing
 @app.route("/reset")
 def reset_session():
     session.clear()
@@ -51,56 +53,94 @@ def dashboard_page():
     return render_template("dashboard.html", user_data=user_data)
 
 
+# Loads event(s) for selecting
 @app.route("/events", methods=["GET", "POST"])
 @check_session()
 def events_page():
     events_data = events.load_events()
+
     if request.method == "GET":
-        if request.args.get('event_num', None):
-            if events_data[int(request.args['event_num'])]:
-                return render_template("event_info.html", event=events_data[int(request.args['event_num'])])
+        confirm_error = request.args.get('confirm_error', "")
+
+        # If an event is selected
+        if request.args.get('num', None):
+            if events_data[int(request.args['num'])]:
+                return render_template(
+                    "event_info.html",
+                    event=events_data[int(request.args['num'])],
+                    confirm_error=confirm_error
+                )
         return render_template("events.html", events_data=events_data)
 
+    # Checks if attendance was confirmed
     if request.method == "POST":
         if request.form.get('confirm_attend', None):
-            users.add_attended(session.get('student_number', None), int(request.args['event_num']))
+            users.add_attended(session.get('student_number', None), int(request.args['num']), )
             return redirect(url_for('events_page'))
+
+        # Displays error not confirmed
         else:
-            return redirect(url_for('events_page', event_num=int(request.args['event_num'])))
+            return redirect(url_for(
+                'events_page',
+                num=int(request.args['num']),
+                confirm_error="Please confirm your attendance before submission."
+            ))
 
 
 # Login and Sign-Up page
 @app.route("/login", methods=["GET", "POST"])
 def login_page():
+
+    # Logged-in users are redirected
     if session.get('student_number'):
         return redirect(url_for('dashboard_page'))
 
+    # Displays login & signup options (and errors, if any)
     if request.method == "GET":
         error_sign_up = request.args.get('error_sign_up', "")
         error_login = request.args.get('error_login', "")
-        return render_template("login.html", error_sign_up=error_sign_up, error_login=error_login)
+        return render_template(
+            "login.html",
+            error_sign_up=error_sign_up,
+            error_login=error_login
+        )
 
+    # On form submission
     if request.method == "POST":
-        if request.form['btn'] == "Sign Up":
+
+        # User tries to login
+        if request.form['btn'] == "Login":
+
+            # If account exists, login
+            if users.check_user(request.form.get('student_number')):
+                session['student_number'] = request.form.get('student_number')
+                return redirect(url_for('dashboard_page'))
+
+            # If account doesn't exist, display error
+            else:
+                return redirect(url_for(
+                    'login_page',
+                    error_login=f"Student Number {request.form.get('student_number')} does not exist."
+                ))
+
+        # User tries to sign up
+        elif request.form['btn'] == "Sign Up":
             new_user_info = {}
             for element_name, value in request.form.items():
                 new_user_info[element_name] = value
             new_user_info.pop('btn')
 
+            # If student number is new, sign up
             if users.add_user(new_user_info):
                 session['student_number'] = request.form.get('student_number')
                 return redirect(url_for('dashboard_page'))
-            else:
-                return redirect(url_for('login_page',
-                                        error_sign_up=f"Student Number {request.form.get('student_number')} is in use."))
 
-        elif request.form['btn'] == "Login":
-            if users.check_user(request.form.get('student_number')):
-                session['student_number'] = request.form.get('student_number')
-                return redirect(url_for('dashboard_page'))
+            # If student number is in use, display error
             else:
-                return redirect(url_for('login_page',
-                                        error_login=f"Student Number {request.form.get('student_number')} does not exist."))
+                return redirect(url_for(
+                    'login_page',
+                    error_sign_up=f"Student Number {request.form.get('student_number')} is in use."
+                ))
 
 
 # Flask app is run, allowing access of the webpage at localhost:8080 in a web browser
